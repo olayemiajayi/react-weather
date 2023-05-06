637166da242056eee5fab4b9e445d48f2da89e8a
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-05-06 13:19:59 -0600

    Created the App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <p>
+        <a
+          rel="noreferrer"
+          href="https://github.com/olayemiajayi/react-weather"
+          target="_blank"
+        >
+          Open Source Code
+        </a>
+        By Yetunde Ajayi
+      </p>
+    </div>
+  );
 }
 
 export default App;
