b2640a830563a8c5d16324e134bcb52a29c8d993
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-05-08 10:54:39 -0600

    created the css file

--- src/Weather.js
+++ src/Weather.js
@@ -4,46 +4,47 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="weather">
-      <form>
-        <div className="form">
-          <input type="search" placeholder="Type city" />
+      <div className="country">
+        <form>
+          <input type="search" placeholder="Type city" autofocus="on" />
           <input
             type="submit"
             value="search"
-            className="btn btn-primary w-2 m-2"
+            className="btn btn-primary w-5 m-2"
           />
-        </div>
-      </form>
+        </form>
 
-      <h1 className="city">London</h1>
-      <div className="row">
-        <div className="col-7">
-          <ul>
-            <li className="day"> Thurday 5:00</li>
-            <li className="days">cloudy</li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-5">
-          <div className="images">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Cloudy"
-            />
-            4°C
+        <h1 className="city">London</h1>
+        <div className="row">
+          <div className="col-7">
+            <ul>
+              <li className="day"> Thurday 5:00</li>
+              <li className="days">cloudy</li>
+            </ul>
           </div>
         </div>
-        <div className="col-5">
-          <div className="wind">
-            <strong>
-              <ul>
-                <li>Percipitation: 13%</li>
-                <li>Humidity:12%</li>
-                <li>Wind: 10Km/h</li>
-              </ul>
-            </strong>
+
+        <div className="row">
+          <div className="col-5">
+            <div className="images">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Cloudy"
+              />
+              <strong>4°C</strong>
+            </div>
+          </div>
+
+          <div className="col-5">
+            <div className="wind">
+              <strong>
+                <ul>
+                  <li>Percipitation:13%</li>
+                  <li>Humidity:12%</li>
+                  <li>Wind: 10Km/h</li>
+                </ul>
+              </strong>
+            </div>
           </div>
         </div>
       </div>
