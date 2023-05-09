36e542ba1198a0ca164b0a0ac89bfd7bce8a3476
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-05-09 12:46:02 -0600

    Added a loader component to the weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import FormattedDate from "./FormattedDate.js";
+import Loader from "react-js-loader";
 import axios from "axios";
 import "./Weather.css";
 
@@ -92,6 +93,10 @@ export default function Weather(props) {
     );
   } else {
     search();
-    return "loading..";
+    return (
+      <div className="loader">
+        <Loader type="bubble-loop" bgColor={"#155263"} size={150} />
+      </div>
+    );
   }
 }
